e427fc431a8bab4c8a4e49017d4ffc36c35521f7
Cabra <greencoder001@outlook.com>
2025-12-23 15:31:11 +0100

    Added SQL client for drizzle

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,10 @@
 import { SQL } from 'bun'
 import { drizzle } from 'drizzle-orm/bun-sql'
+import config from './config'
 
-const client = new SQL()
+export const client = new SQL(
+  `postgres://${config.database.user}:${config.database.password}@${config.database.host}:${config.database.port}/${config.database.db}`
+)
 
 const db = drizzle({ client })
 

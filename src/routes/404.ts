67911a1da6f0f3726caa368630f70e01bd6cbdaa
Cabra <64889695+cabraviva@users.noreply.github.com>
2025-12-26 19:30:17 +0100

    / and 404 are hidden in OAPI now

--- src/routes/404.ts
+++ src/routes/404.ts
@@ -1,6 +1,20 @@
 import fastify from '../fastify'
+import * as z from 'zod'
 
-fastify.get('*', (req, res) => {
-    res.status(404)
-    res.send('Sorry, but this page does not exist.')
-})
+fastify.get(
+    '*',
+    {
+        schema: {
+            response: {
+                404: z.string().meta({
+                    description: 'A 404 Not Found error message or page.'
+                })
+            },
+            hide: true // -> Hides it in the OpenAPI.json spec
+        }
+    },
+    (req, res) => {
+        res.status(404)
+        res.send('Sorry, but this page does not exist.')
+    }
+)

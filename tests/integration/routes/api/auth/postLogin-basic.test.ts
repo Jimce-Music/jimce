0de4090cbe57754b6c3fb6b9c1cbadf45e7fdd14
Cabra <64889695+cabraviva@users.noreply.github.com>
2026-01-10 09:52:59 +0100

    Test wrong credentials on login basic

--- tests/integration/routes/api/auth/postLogin-basic.test.ts
+++ tests/integration/routes/api/auth/postLogin-basic.test.ts
@@ -23,6 +23,35 @@ describe('POST /api/auth/login-basic', () => {
         expect(data.token).toBeString()
         expect(data.token).toSatisfy((val) => val.length > 8)
     })
+
+    test('Fail on wrong username', async () => {
+        const res = await fastify.inject({
+            method: 'post',
+            url: '/api/auth/login-basic',
+            body: {
+                username: 'not.a.existing.user',
+                password: '123456789abc'
+            }
+        })
+
+        // Status code
+        expect(res.statusCode).toBe(401)
+    })
+
+    test('Fail on wrong password', async () => {
+        const res = await fastify.inject({
+            method: 'post',
+            url: '/api/auth/login-basic',
+            body: {
+                username: 'admin',
+                password: 'definitely-the-wrong-password'
+            }
+        })
+
+        // Status code
+        expect(res.statusCode).toBe(401)
+    })
 })
 
 // TODO: Implement test components which can be reused for routes, like checking whether auth and permissions are ensured, and data is validated
+// TODO: Auto-apply component tests on route/code generation

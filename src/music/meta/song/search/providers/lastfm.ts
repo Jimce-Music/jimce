cc0e6cdb9c06b1521217d813776ee6eaa458cb9a
Cabra <greencoder001@outlook.com>
2026-03-02 19:35:27 +0100

    Tried last.fm duration fetching

--- src/music/meta/song/search/providers/lastfm.ts
+++ src/music/meta/song/search/providers/lastfm.ts
@@ -19,20 +19,31 @@ export default async function findByQuery(
     const adaptedResults: GenericSearchSchemeT[] = []
 
     for (const result of results.results.trackmatches.track) {
+        // const trackInfo = await trackApi.getInfo({
+        //     track: result.name,
+        //     artist: result.artist
+        // })
+        // const duration = parseInt(trackInfo.track.duration)
+        // console.log(trackInfo.track.duration)
+
         adaptedResults.push({
             providedBy: 'lastfm',
 
             title: result.name,
 
+            // length: duration !== 0 ? duration : undefined,
+
             // Hints make it easier for metadata / sound matchers to determine a match based on ids or urls
             hints: {
                 lastfm: {
                     mbid: result.mbid.length === 36 ? result.mbid : undefined,
-                    artistString: result.artist
+                    artistString: result.artist,
                 }
             }
         })
     }
+
+    // Consideration: Now keep results in order, but prefer results with mbid (show first), as they tend to be more popular
 
     return adaptedResults
 }

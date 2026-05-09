e9fa5312ecf76beb48729280ae320b80ba26d671
Cabra <greencoder001@outlook.com>
2026-05-09 11:34:10 +0200

    added streamable route for song search

--- src/routes/api/search/streamed/getSearch-songs.ts
+++ src/routes/api/search/streamed/getSearch-songs.ts
@@ -24,6 +24,8 @@ import requireJWT from '../../../../types/requireJWT'
 import { JWTPayloadZ } from '../../../../types/JWTPayload'
 import SuccessIndicatorResponseZ from '../../../../types/SuccessIndicatorResponseZ'
 import searchSongs from '../../../../music/meta/searchSongs'
+import type { SRLExtenderPacket, SRLInitPacket } from 'streamed-result-list'
+import type { JimceSongSearchResult } from '../../../../music/meta/JimceSearchResult'
 
 fastify.withTypeProvider<FastifyZodOpenApiTypeProvider>().get(
     '/api/search/streamed/search-songs',
@@ -32,16 +34,18 @@ fastify.withTypeProvider<FastifyZodOpenApiTypeProvider>().get(
 
         schema: {
             hide: false,
-            summary: '', // TODO: Add summary and description
-            description: ``, // Expandable, more detailed description
-            // security: requireJWT,
+            summary: 'Search for songs (STREAMED)', // TODO: Add summary and description
+            description: `This allows to search for songs by a specified query and returns multiple, streamed lines of json.
+This route is not supported by the normal OpenAPI clients but should be used in combination with fetch and the official https://github.com/Jimce-Music/streamed-result-list package, which allows parsing the streamed result back into a list.
+This also simplifies usage with react (states).`, // Expandable, more detailed description
+            security: requireJWT,
 
             querystring: z.object({
                 q: z.string().min(1)
             }),
 
             response: {
-                200: SuccessIndicatorResponseZ,
+                200: z.any(),
                 400: BadRequestResponseZ,
                 401: UnauthorizedResponseZ,
 
@@ -50,24 +54,24 @@ fastify.withTypeProvider<FastifyZodOpenApiTypeProvider>().get(
         } satisfies FastifyZodOpenApiSchema
     },
     async (req, res) => {
-        // try {
-        //     let user: z.infer<typeof JWTPayloadZ>
-        //     try {
-        //         user = JWTPayloadZ.parse(req.user)
-        //     } catch (err) {
-        //         logger.warn('Error during JWT payload parsing via zod:')
-        //         logger.warn(err)
-        //         return res.status(401).send({
-        //             statusCode: 401,
-        //             error: 'Unauthorized',
-        //             message: 'Failed to parse token payload',
-        //             code: 'TOKEN_PAYLOAD_INVALID'
-        //         })
-        //     }
-        //     res.status(200).send({ success: true })
-        // } catch (err) {
-        //     return failInternal(res, err)
-        // }
+        try {
+            let user: z.infer<typeof JWTPayloadZ>
+            try {
+                user = JWTPayloadZ.parse(req.user)
+            } catch (err) {
+                logger.warn('Error during JWT payload parsing via zod:')
+                logger.warn(err)
+                return res.status(401).send({
+                    statusCode: 401,
+                    error: 'Unauthorized',
+                    message: 'Failed to parse token payload',
+                    code: 'TOKEN_PAYLOAD_INVALID'
+                })
+            }
+            res.status(200).send({ success: true })
+        } catch (err) {
+            return failInternal(res, err)
+        }
 
         res.type('application/jsonl')
         res.header('Transfer-Encoding', 'chunked')
@@ -79,14 +83,44 @@ fastify.withTypeProvider<FastifyZodOpenApiTypeProvider>().get(
         // Wait for search to complete, then return results
         try {
             await new Promise<void>(async (resolve) => {
-                const initPacket = searchResults.requestInitPacket()
-                const ok = stream.write(`${JSON.stringify(initPacket)}\n`)
-                if (!ok) await new Promise((res) => stream.once('drain', res))
+                let writeChain = Promise.resolve() // start with a resolved promise
 
-                searchResults.onExtenderPacket(async (packet) => {
-                    const ok = stream.write(`${JSON.stringify(packet)}\n`)
-                    if (!ok)
-                        await new Promise((res) => stream.once('drain', res)) // TODO: handle this properly (not waiting right now id say)
+                function writePacket(
+                    packet:
+                        | SRLInitPacket<JimceSongSearchResult>
+                        | SRLExtenderPacket<JimceSongSearchResult>
+                ) {
+                    // Append this packet to the chain
+                    writeChain = writeChain.then(() => {
+                        if (
+                            stream.closed ||
+                            stream.writableEnded ||
+                            stream.destroyed
+                        ) {
+                            logger.warn(
+                                'streamed search: stream already closed; will not write data'
+                            )
+                            stream.destroy()
+                            resolve()
+                            return
+                        }
+                        const ok = stream.write(`${JSON.stringify(packet)}\n`)
+                        if (ok) return // immediate success
+                        return new Promise((res) => stream.once('drain', res)) // wait for queue / drain
+                    })
+                }
+
+                const initPacket = searchResults.requestInitPacket()
+                writePacket(initPacket)
+
+                stream.on('close', () => {
+                    stream.destroy()
+                    resolve()
+                    return
+                })
+
+                searchResults.onExtenderPacket((packet) => {
+                    writePacket(packet)
                 })
 
                 searchResults.onClose(() => {
